25b37cfe2ee7ddf3f93b075c9f037c38889c1fa4
James Gardiner <jamesgardiner075@gmail.com>
2016-11-13 23:46:20 +0000

    Add createArticles feature

--- src/articles-model.js
+++ src/articles-model.js
@@ -1,13 +1,22 @@
 (function(exports) {
+  var items = [];
+
   function Articles() {
-    this.items = [];
+    this.items = items;
   }
 
-  Articles.prototype.getArticles = function (requestAPI) {
+  Articles.prototype.getArticles = function (article, requestAPI) {
     requestAPI(function(response) {
-      createArticles(response);
+      createArticles(article, response);
     });
   };
+
+  function createArticles(article, responseObject) {
+    responseObject.response.results.forEach(function (value) {
+      var formattedArticle = new article(value.webTitle, value.apiUrl, null);
+      items.push(formattedArticle);
+    });
+  }
 
   exports.Articles = Articles;
 })(this);
--- tests/articles-tests.js
+++ tests/articles-tests.js
@@ -1,13 +1,58 @@
 articles = new Articles();
 
+var guardianAPISample = {
+  "response":{
+    "status":"ok",
+    "userTier":"developer",
+    "total":1906643,
+    "startIndex":1,
+    "pageSize":10,
+    "currentPage":1,
+    "pages":190665,
+    "orderBy":"newest",
+    "results":[
+      {
+        "id":"sport/live/2016/nov/13/f1-brazilian-grand-prix-live",
+        "type":"liveblog",
+        "sectionId":"sport",
+        "sectionName":"Sport",
+        "webPublicationDate":"2016-11-13T17:26:05Z",
+        "webTitle":"F1: Brazilian Grand Prix – live!",
+        "webUrl":"https://www.theguardian.com/sport/live/2016/nov/13/f1-brazilian-grand-prix-live",
+        "apiUrl":"https://content.guardianapis.com/sport/live/2016/nov/13/f1-brazilian-grand-prix-live",
+        "isHosted":false
+      }, {
+            "id": "world/live/2016/nov/14/new-zealand-earthquake-evacuations-tsnuami-east-coast-south-north-islands",
+            "type": "liveblog",
+            "sectionId": "world",
+            "sectionName": "World news",
+            "webPublicationDate": "2016-11-13T17:25:48Z",
+            "webTitle": "New Zealand earthquake: evacuations as tsunami heads for east coast – live",
+            "webUrl": "https://www.theguardian.com/world/live/2016/nov/14/new-zealand-earthquake-evacuations-tsnuami-east-coast-south-north-islands",
+            "apiUrl": "https://content.guardianapis.com/world/live/2016/nov/14/new-zealand-earthquake-evacuations-tsnuami-east-coast-south-north-islands",
+            "isHosted": false
+        }
+    ]
+  }
+};
+
 function articlesItemsAreEmpty() {
   assert.isTrue(articles.items.length === 0);
 }
 
 function articlesMakesCallToAPI() {
   requestAPI = spy.On;
-  assert.makesCall([articles.getArticles, [requestAPI]], requestAPI);
+  assert.makesCall([articles.getArticles, [Article, requestAPI]], requestAPI);
+}
+
+function articlesAddsArticleObjects() {
+  requestAPI = function(fn) {
+    fn(guardianAPISample);
+  };
+  articles.getArticles(Article, requestAPI);
+  assert.isTrue(articles.items.length > 0);
 }
 
 articlesItemsAreEmpty();
 articlesMakesCallToAPI();
+articlesAddsArticleObjects();
